1a540fe5232f83d871fab336457cb11d47ce6d56
Tyler Z <zambam5ttv@gmail.com>
2024-06-16 17:50:50 -0500

    Updating to new changes

--- beaverbot.py
+++ beaverbot.py
@@ -1,9 +1,7 @@
 # noinspection PyUnresolvedReferences
 import cfgzam as cfg
 import time, logging, random, json, requests, socket, re
-import spotipy
-import spotipy.util as util
-from messages import MessageHandler
+from messages import Message
 import song_requests as sr
 from irctools import chat, ping
 
@@ -11,14 +9,16 @@ from irctools import chat, ping
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-handler = logging.FileHandler('beaver.log', mode='w')
-formatter = logging.Formatter('''%(asctime)s -
-                              %(name)s - %(levelname)s - %(message)s''')
+handler = logging.FileHandler("beaver.log", mode="w")
+formatter = logging.Formatter(
+    """%(asctime)s - %(name)s - %(levelname)s - %(message)s"""
+)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-Messages = MessageHandler(cfg.CHAN)
-s = None #only here to make the linter happy
+
+s = None  # only here to make the linter happy
+
 
 def connect(HOST, PORT):
     global s
@@ -31,7 +31,7 @@ def connect(HOST, PORT):
     try:
         s.connect((HOST, PORT))
     except ConnectionAbortedError:
-        logger.info('Connection Failed')
+        logger.info("Connection Failed")
 
 
 def login(sock, PASS, NICK, CHAN):
@@ -60,68 +60,82 @@ def exponential_backoff():
             connect(cfg.HOST, cfg.PORT)
             login(s, cfg.PASS, cfg.NICK, cfg.CHAN)
             return True
-        except socket.error:
+        except ConnectionResetError:
             time.sleep(count)
-            count = count*2
+            count = count * 2
+
+
+def splitmessages(response: str):
+    messages = response.splitlines()
+    return messages
 
 
 command_switch = {
-        '!clearplaylist': lambda : sr.clear_playlist(s, messagedict),
-        '!requeue': lambda : sr.requeue(s, messagedict),
-        '!modqueue': lambda : sr.mod_queue(s, messagedict),
-        '!song': lambda : sr.now_playing(s, messagedict)
+    "!clearplaylist": lambda: sr.clear_playlist(s, messagedict),
+    "!requeue": lambda: sr.requeue(s, messagedict),
+    "!modqueue": lambda: sr.mod_queue(s, messagedict),
+    "!song": lambda: sr.now_playing(s, messagedict),
 }
 
-#message type RECONNECT
+# message type RECONNECT
 def reconnect(HOST, PORT, PASS, NICK, CHAN):
     global s
+    s.close()
     connect(HOST, PORT)
     login(s, PASS, NICK, CHAN)
 
 
-#message type HOSTTARGET
+# message type HOSTTARGET
 def host(sock, message):
-    target = message['host target']
-    if target == '-':
+    target = message["host target"]
+    if target == "-":
         return False
     else:
-        return chat(s, f"If the host broke on your end, here is the link: https://twitch.tv/{target}", cfg.CHAN)
+        return chat(
+            s,
+            f"If the host broke on your end, here is the link: https://twitch.tv/{target}",
+            cfg.CHAN,
+        )
 
 
-#message type PRIVMSG
+# message type PRIVMSG
 def PRIVMSG(mtesting, messagedict):
-    userid = messagedict['user-id']
-    username = messagedict['user-id']
-    if 'custom-reward-id' in messagedict.keys():
+    userid = messagedict["user-id"]
+    username = messagedict["user-id"]
+    if "custom-reward-id" in messagedict.keys():
         reward_switch = {
-            '16f48209-c3b9-4a32-9143-109a2802a162': lambda : sr.song_requests(s, messagedict, cfg.URL)
+            "16f48209-c3b9-4a32-9143-109a2802a162": lambda: sr.song_requests(
+                s, messagedict, cfg.URL
+            )
         }
-        mtesting = reward_switch.get(messagedict['custom-reward-id'], lambda : False)()
-    elif messagedict['actual message'].startswith('!'):
-        word_list = messagedict['actual message'].split(" ")
+        mtesting = reward_switch.get(messagedict["custom-reward-id"], lambda: False)()
+    elif messagedict["message"].startswith("!"):
+        word_list = messagedict["message"].split(" ")
         command = word_list[0]
-        mtesting = command_switch.get(command, lambda : False)()
+        mtesting = command_switch.get(command, lambda: False)()
     return mtesting
 
-#message type: userstate, clearchat, clearmsg, notice, roomstate, usernotice, whisper
-def the_rest(): return False
+
+# message type: userstate, clearchat, clearmsg, notice, roomstate, usernotice, whisper
+def the_rest():
+    return False
+
 
 message_switch = {
-            "WHISPER": lambda : the_rest(),
-            "PRIVMSG": lambda : PRIVMSG(mtesting, messagedict),
-            "USERNOTICE": lambda : the_rest(),
-            "USERSTATE": lambda : the_rest(),
-            "CLEARCHAT": lambda : the_rest(),
-            "CLEARMSG": lambda : the_rest(),
-            "HOSTTARGET": lambda : host(s, messagedict),
-            "NOTICE": lambda : the_rest(),
-            "ROOMSTATE": lambda : the_rest(),
-            "PING": lambda : ping(s),
-            "RECONNECT": lambda : reconnect(cfg.HOST, cfg.PORT, cfg.PASS, cfg.NICK, cfg.CHAN)
-        }
+    "WHISPER": lambda: the_rest(),
+    "PRIVMSG": lambda: PRIVMSG(mtesting, messagedict),
+    "USERNOTICE": lambda: the_rest(),
+    "USERSTATE": lambda: the_rest(),
+    "CLEARCHAT": lambda: the_rest(),
+    "CLEARMSG": lambda: the_rest(),
+    "HOSTTARGET": lambda: host(s, messagedict),
+    "NOTICE": lambda: the_rest(),
+    "ROOMSTATE": lambda: the_rest(),
+    "PING": lambda: ping(s),
+    "RECONNECT": lambda: reconnect(cfg.HOST, cfg.PORT, cfg.PASS, cfg.NICK, cfg.CHAN),
+}
 
 if __name__ == "__main__":
-
 
     connect(cfg.HOST, cfg.PORT)
     login(s, cfg.PASS, cfg.NICK, cfg.CHAN)
@@ -141,22 +155,24 @@ if __name__ == "__main__":
             continue
         if response == "":
             continue
-        messagelist = Messages.splitmessages(response)
+        messagelist = splitmessages(response)
         for message in messagelist:
             try:
                 mtesting = False
                 try:
-                    messagedict = Messages.message_dict_maker(message)
+                    messagedict = Message(message).dict
                 except:
                     logging.exception(response)
                     continue
-                if not messagedict:
-                    print('bad message')
+                if messagedict["action"] == "BAD MESSAGE":
+                    print("bad message")
                     continue
                 else:
-                    mtesting = message_switch.get(messagedict['message type'], lambda : False)()
+                    mtesting = message_switch.get(
+                        messagedict["action"], lambda: False
+                    )()
                 if mtesting:
-                    time.sleep(1/(100/30))
+                    time.sleep(1 / (100 / 30))
             except:
                 print(message)
                 logger.exception("Error processing message:" + message)
--- messages.py
+++ messages.py
@@ -1,71 +1,105 @@
 from datetime import datetime
+import re
 import logging
+from typing import Any
+from typing import Dict
+from typing import Optional
 
-logger = logging.getLogger('__main__.' + __name__)
+logger = logging.getLogger("__main__." + __name__)
 
-class MessageHandler():
-    def __init__(self, chan):
-        self.chan = chan
-    
 
-    def splitmessages(self, response):
-        messages = response.splitlines()
-        return messages
-    
+class Message:
+    action: str
+    raw_message: Optional[str]
+    data: Optional[str]
+    name: Optional[str]
+    message: str
+    channel: Optional[str]
+    custom_reward_id: Optional[str]
 
-    def message_dict_maker(self, message):
-        messagedict = dict()
-        if message.startswith('PING'):
-            messagedict['message type'] = 'PING'
-            messagedict['time'] = datetime.now()
+    regex = {
+        "data": re.compile(
+            r"^(?:@(?P<tags>\S+)\s)?:(?P<data>\S+)(?:\s)"
+            r"(?P<action>[A-Z]+)(?:\s#)?(?P<channel>\S+)?"
+            r"(?:\s(?::)?(?P<content>.+))?"
+        ),
+        "ping": re.compile(r"PING (?P<content>.+)"),
+        "reconnect": re.compile(r"RECONNECT"),
+        "author": re.compile(
+            r"(?P<author>[a-zA-Z0-9_]+)!(?P=author)" r"@(?P=author).tmi.twitch.tv"
+        ),
+        "mode": re.compile(r"(?P<mode>[\+\-])o (?P<user>.+)"),
+        "host": re.compile(r"(?P<channel>[a-zA-Z0-9_]+) " r"(?P<count>[0-9\-]+)"),
+    }
+
+    def __init__(self, message) -> None:
+        self.raw_message = message
+        self.message = None
+        self.process_message(message)
+
+    def process_message(self, message: str) -> None:
+        """Take a message from the client and build the class from it
+
+        Args:
+            message (str): A string from the Twitch IRC client
+        """
+        messagedict = self._create_message_dict(message)
+        if not messagedict:
+            self.action = "BAD MESSAGE"
+            self.channel = None
+        else:
+            self.dict = messagedict
+            for key in messagedict:
+                key_ = key.replace("-", "_")
+                setattr(self, key_, messagedict[key])
+
+    def _create_message_dict(self, message: str):
+        """
+        uses regex to process a message
+        some messages come broken
+        currently no way to deal with that
+        """
+        messagedict = {}
+        if message.startswith("PING"):
+            messagedict["action"] = "PING"
+            messagedict["time"] = datetime.now()
             return messagedict
-        elif 'user-type=' in message:
-            messagelist1 = message.split('user-type=')
-            messagelist = messagelist1[0].split(';')
-            for item in messagelist:
-                items = item.split('=')
+        else:
+            m = self.regex["data"].match(message)
+            try:
+                tags = m.group("tags")
+                for tag in tags.split(";"):
+                    t = tag.split("=")
+                    messagedict[t[0]] = t[1]
+            except:
+                tags = None
+
+            try:
+                action = m.group("action")
+                messagedict["action"] = action
+            except:
+                action = None
+
+            try:
+                data = m.group("data")
+                messagedict["data"] = data
                 try:
-                    messagedict[items[0]] = items[1]
-                except IndexError:
-                    messagedict[items[0]] = ''
-            if "PRIVMSG" in messagelist1[1]:
-                messagedict['message type'] = "PRIVMSG"
-                firstsplit = messagelist1[1].split(" " + self.chan + " :")
-                if firstsplit[0].startswith('mod :'):
-                    namestrip = firstsplit[0][5:]
-                else:
-                    namestrip = firstsplit[0][2:]
-                name = namestrip.split('!')[0]
-                messagedict['name'] = name
-                messagedict['actual message'] = firstsplit[1].strip(":")
-            elif "WHISPER" in messagelist1[1]:
-                print("whisper")
-                messagedict['message type'] = "WHISPER"
-            elif "PRIV1MSG" in messagelist1[1]:
-                messagedict['message type'] = "PRIVMSG"
-                firstsplit = messagelist1[1].split(" " + self.chan + " :")
-                messagedict['actual message'] = firstsplit[1].strip(':')
-            elif "USERNOTICE" in messagelist1[1]:
-                messagedict['message type'] = "USERNOTICE"
-            elif "USERSTATE" in messagelist1[1]:
-                messagedict['message type'] = "USERSTATE"
-            return messagedict
-        if message.startswith('badge'):
-            return False
-        messagelist1 = message.split(' ')
-        print(messagelist1)
-        if messagelist1[1] == "HOSTTARGET":
-            messagedict['message type'] = 'HOSTTARGET'
-            messagedict['host target'] = messagelist1[3].strip(':')
-        elif messagelist1[1] == "RECONNECT":
-            messagedict['message type'] = 'RECONNECT'
-        elif messagelist1[2] == "NOTICE":
-            messagedict['message type'] = "NOTICE"
-        elif messagelist1[2] == "CLEARCHAT":
-            messagedict['message type'] = "CLEARCHAT"
-        elif messagelist1[2] == "ROOMSTATE":
-            messagedict['message type'] = "ROOMSTATE"
-        elif messagelist1[2] == "CLEARMSG":
-            print('here')
-            messagedict['message type'] = "CLEARMSG"
+                    name = self.regex["author"].match(data).group("author")
+                    messagedict["name"] = name
+                except:
+                    name = None
+            except:
+                data = None
+
+            try:
+                content = m.group("content")
+                messagedict["message"] = content
+            except:
+                content = None
+
+            try:
+                channel = m.group("channel")
+                messagedict["channel"] = channel
+            except:
+                channel = None
         return messagedict
